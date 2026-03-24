a93914a996315e1cae4e8a06df558bbb0593b803
toannp02 <ngtoan554@gmail.com>
2026-03-24 09:12:48 +0700

    update withdraw

--- src/api/useWithdraw.service.ts
+++ src/api/useWithdraw.service.ts
@@ -1,1 +1,25 @@
-// This file has been removed per user request to restore frontend to previous state.
+import useHttpClient from "./useHttpClient";
+import { WithdrawRequest, WithdrawResponse } from "@/types/auth";
+import { useMemo } from "react";
+
+const useWithdrawService = () => {
+  const httpClient = useHttpClient();
+
+  const createWithdrawRequest = async (payload: WithdrawRequest): Promise<WithdrawResponse> => {
+    const response: any = await httpClient.post("/withdraw-requests", payload);
+    return response.data;
+  };
+
+  const getMyWithdrawRequests = async (): Promise<WithdrawResponse[]> => {
+    const response: any = await httpClient.get("/withdraw-requests/my");
+    return response.data;
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(() => ({
+    createWithdrawRequest,
+    getMyWithdrawRequests,
+  }), [httpClient]);
+};
+
+export default useWithdrawService;
